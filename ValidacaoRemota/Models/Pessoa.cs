d053dbcb421100d1b8cad212a457eb5ead6184e9
Iaritzza Bielinki <iaritzzayara@gmail.com>
2021-09-03 10:37:25 -0300

    Projeto Criado

---
+++ ValidacaoRemota/Models/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ValidacaoRemota.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+
+        [Remote("UsuarioExisteAsync", "Pessoas")]
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
